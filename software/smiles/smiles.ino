24765deff8ac8a9a5faa5fea46aa299a695ce450
Ichi Kanaya <kanaya@pineapple.cc>
2020-05-14 16:49:00 +0900

    The second try.

--- software/smiles/smiles.ino
+++ software/smiles/smiles.ino
@@ -1,8 +1,38 @@
 #include <Adafruit_NeoPixel.h>
 
-const int MAX_VAL = 128;
+const int MAX_VAL = 64;
 const int N_STEPS = 16;
 const int DELTA = MAX_VAL / N_STEPS;
+
+const int NP_PIN = 6;
+const int N_LEDS = 27;
+
+const int FADE_IN = 0;
+const int FADE_OUT = 1;
+
+const int HAPPY[] = {
+    0,  0,  0,  0,  0,
+  1,  0,  0,  0,  0,  1,
+    1,  0,  0,  0,  1,
+  0,  1,  1,  1,  1,  0,
+    0,  0,  0,  0,  0,
+};
+
+const int BAD[] = {
+    0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,
+    0,  1,  1,  1,  0,
+  0,  1,  0,  0,  1,  0,
+    1,  0,  0,  0,  1,
+};
+
+const int SAD[] = {
+    0,  1,  0,  1,  0,
+  0,  0,  1,  1,  0,  0,
+    0,  0,  1,  0,  0,
+  0,  0,  1,  1,  0,  0,
+    0,  1,  0,  1,  0,
+};
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
@@ -11,21 +41,26 @@ const int DELTA = MAX_VAL / N_STEPS;
 //   NEO_GRB     Pixels are wired for GRB bitstream
 //   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
 //   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(23, 6, NEO_GRB | NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, NP_PIN, NEO_GRB | NEO_KHZ800);
 
 void setup() {
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
-  clear();
 }
 
 void loop() {
-  happy();
+  fade(0, MAX_VAL, 0, HAPPY, FADE_IN);
   delay(2000);
-  unhappy();
-  sad();
+  fade(0, MAX_VAL, 0, HAPPY, FADE_OUT);
   delay(2000);
-  unsad();
+  fade(MAX_VAL, MAX_VAL, 0, BAD, FADE_IN);
+  delay(2000);
+  fade(MAX_VAL, MAX_VAL, 0, BAD, FADE_OUT);
+  delay(2000);
+  fade(MAX_VAL, 0, 0, SAD, FADE_IN);
+  delay(2000);
+  fade(MAX_VAL, 0, 0, SAD, FADE_OUT);
+  delay(2000);
 }
 
 void clear() {
@@ -35,46 +70,28 @@ void clear() {
   strip.show();
 }
 
-void happy() {
+void fade(int target_r, int target_g, int target_b, int target_pattern[], int in_or_out) {
+  int delta_r = target_r / N_STEPS;
+  int delta_g = target_g / N_STEPS;
+  int delta_b = target_b / N_STEPS;
   for (int t = 0; t < N_STEPS; ++t) {
-    for (int i = 0; i < 14; ++i) {
-      int c = DELTA * t;
-      strip.setPixelColor(i, strip.Color(0, c, 0));
+    int r, g, b;
+    if (in_or_out == FADE_IN) {
+      r = delta_r * t;
+      g = delta_g * t;
+      b = delta_b * t;
+    }
+    else {
+      r = target_r - delta_r * (t + 1);
+      g = target_g - delta_g * (t + 1);
+      b = target_b - delta_b * (t + 1);
+    }
+    for (int i = 0; i < N_LEDS; ++i) {
+      if (target_pattern[i] == 1) {
+        strip.setPixelColor(i, strip.Color(r, g, b));
+      }
     }
     strip.show();
     delay(100);
   }
 }
-
-void unhappy() {
-  for (int t = 0; t < N_STEPS; ++t) {
-    for (int i = 0; i < 14; ++i) {
-      int c = MAX_VAL - DELTA * (t + 1);
-      strip.setPixelColor(i, strip.Color(0, c, 0));
-    }
-    strip.show();
-    delay(100);
-  }
-} 
-
-void sad() {
-  for (int t = 0; t < N_STEPS; ++t) {
-    for (int i = 14; i < 23; ++i) {
-      int c = DELTA * t;
-      strip.setPixelColor(i, strip.Color(c, 0, 0));
-    }
-    strip.show();
-    delay(100);
-  }
-}
-
-void unsad() {
-  for (int t = 0; t < N_STEPS; ++t) {
-    for (int i = 14; i < 23; ++i) {
-      int c = MAX_VAL - DELTA * (t + 1);
-      strip.setPixelColor(i, strip.Color(c, 0, 0));
-    }
-    strip.show();
-    delay(100);
-  }
-} 
